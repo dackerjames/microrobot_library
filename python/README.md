3d748bcde45d59b4624bb116d7d2ab23dc7c2cdb
dteal <dteal@dteal.org>
2021-04-05 23:29:56 -0700

    switches to gdspy library

---
+++ python/README.md
@@ -0,0 +1,20 @@
+# python microrobot_library
+
+This is the potential beginning of a python library designed to aid in MEMS microrobot design and layout that works similarly to the MATLAB library.  
+
+This repository is built upon (and requires) the `gdspy` library (https://github.com/heitzmann/gdspy), a python library capable of manipulating and generating GDSII files.
+
+(Note that `gdspy` library is planned to be deprecated in the future in favor of `gdstk` by the same authors. `gdstk` uses compiled c code for extra speed, but also requires installation of the LAPACK library.)
+
+## More detailed installation instructions
+
+`gdspy` can be installed by using a command line window to run:
+
+```
+pip install numpy
+pip install gdspy
+```
+
+Running `test.py` should produce test GDSII and SVG files.
+
+Generated GDSII files can be opened and edited with a GDSII editor like [KLayout](https://klayout.de/).
--- python/test.py
+++ python/test.py
@@ -1,25 +1,33 @@
 #!/usr/bin/env python3
 """
-This is a test script to generate microrobot parts with the gdstk library.
+This is a test script to generate microrobot parts with the gdspy library.
 
-Right now, this follows directly from the gdstk documentation at
-https://heitzmann.github.io/gdstk/index.html .
+The gdspy library will eventually be superseded by the gdstk library,
+which is similar but uses significant compiled c code for faster speeds.
+However, it requires LAPACK, which is currently not straightforward to
+install on Windows.
+
+Right now, this script follows directly from the gdspy documentation at
+https://gdspy.readthedocs.io/en/stable/gettingstarted.html .
 """
 
-import gdstk
+import gdspy
 
-# The GDSII file is called a library, which contains multiple cells.
-lib = gdstk.Library()
+# The GDSII file is a library, which contains multiple cells.
+lib = gdspy.GdsLibrary()
 
 # Geometry must be placed in cells.
-cell = lib.new_cell("FIRST")
+cell = lib.new_cell('FIRST')
 
 # Create the geometry (a single rectangle) and add it to the cell.
-rect = gdstk.rectangle((0, 0), (2, 1))
+rect = gdspy.Rectangle((0, 0), (2, 1))
 cell.add(rect)
 
-# Save the library in a GDSII file.
-lib.write_gds("first.gds")
+# Save the library in a file called 'first.gds'.
+lib.write_gds('first.gds')
 
 # Optionally, save an image of the cell as SVG.
-cell.write_svg("first.svg")
+cell.write_svg('first.svg')
+
+# Display all cells using the internal viewer.
+gdspy.LayoutViewer(lib)
